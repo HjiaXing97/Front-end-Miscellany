2b8f80bd44aae75e403a415edde1950109bf047e
hovercao <17388766232@163.com>
2022-12-30 16:15:17 +0800

    ✨新增: 添加icon

--- src/views/LowCode/data/ElementList.ts
+++ src/views/LowCode/data/ElementList.ts
@@ -1,36 +1,44 @@
 interface IElement {
   key: string;
   label: string;
+  id: string;
 }
 
 const ElementList: IElement[] = [
   {
     key: "input",
+    id: "input",
     label: "输入框"
   },
   {
     key: "select",
-    label: "选择框"
+    label: "选择框",
+    id: "select"
   },
   {
     key: "inputNumber",
-    label: "数字输入框"
+    label: "数字输入框",
+    id: "inputNumber"
   },
   {
     key: "datePicker",
-    label: "日期选择"
+    label: "日期选择",
+    id: "datePicker"
   },
   {
     key: "textArea",
-    label: "多行文本"
+    label: "多行文本",
+    id: "textArea"
   },
   {
     key: "radio",
-    label: "单选"
+    label: "单选",
+    id: "radio"
   },
   {
     key: "switch",
-    label: "开关"
+    label: "开关",
+    id: "switch"
   }
 ];
 
